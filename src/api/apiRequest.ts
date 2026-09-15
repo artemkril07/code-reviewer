5113481256184ea54a948c3b9a49a32062a81662
Artem Kril <arturkril9@gmail.com>
2026-09-15 18:49:45 +0300

    AI: Implemented ai assistant for code review. Added window for response

---
+++ src/api/apiRequest.ts
@@ -0,0 +1,37 @@
+import.meta.env;
+
+// Structure of api request on Gemini server
+
+export const sendRequest = async (request: string) => {
+  const url: string =
+    "https://openrouter.ai/api/v1/chat/completions";
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${ import.meta.env.VITE_OPEN_ROUTER_API_KEY}`,
+      'HTTP-Referer': 'http://localhost:5173/',
+      'X-Title': 'Code Reviewer',
+    },
+     body: JSON.stringify({
+    model: 'qwen/qwen-2.5-coder-32b-instruct',
+    messages: [
+      {
+        role: 'user',
+        content: request,
+      },
+    ],
+  }),
+  });
+
+  // console.log(import.meta.env.VITE_OPEN_ROUTER_API_KEY);
+  
+  // Checking if there is an error with response from server
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.error?.message || "There is an technical issue");
+  }
+  return response;
+};
--- src/components/CodeInput.tsx
+++ src/components/CodeInput.tsx
@@ -1,37 +1,83 @@
-import {  useState, type FC } from "react";
+import { useState, type FC } from "react";
 import { type CodeInputProps } from "../types/index.ts";
+import { sendRequest } from "../api/apiRequest.ts";
 
 export const CodeInput: FC<CodeInputProps> = () => {
+  const [inputValue, setInputValue] = useState<string>("");
+  const [aiResponse, setAiResponse] = useState<string>("");
+  const [errorRequest, setErrorRequest] = useState<string>("")
 
-  const [inputValue, setInputValue] = useState <string>("");
-
-  const controleCode = (e) =>{
+  const controlCode = (e) => {
     setInputValue(e.target.value);
     console.log(e.target.value);
-  }
-  const saveCode=()=>{
-    localStorage.setItem('userCode', JSON.stringify(inputValue));
-  }
+  };
+
+  const handleClick = async () => {
+    setAiResponse("")
+    localStorage.setItem("userCode", JSON.stringify(inputValue));
+    try {
+      const response = await sendRequest(inputValue);
+      const data = await response.json();
+      console.log(data);
+      
+      if (data.choices && data.choices.length > 0){
+        const responseDataAi = data.choices[0].message.content
+        setAiResponse(responseDataAi);
+        console.log(responseDataAi);
+      }else{
+        setErrorRequest('Помилка сервера');
+        
+      }
+      
+    } catch (error) {
+      console.error(error.message);
+      setErrorRequest(error.message);
+    }
+  };
 
   return (
     <div>
-      <div className="flex-container mt-20 p-8">
-        <h1 className="text-default uppercase">Code Reviewer</h1>
-        <h6 className="pl-30">by dr.Cactus</h6>
-        <div className="flex flex-col w-fit gap-4 mt-20">
+      <div className="">
+        <h1 className="text-default uppercase text-center mt-10">Code Reviewer</h1>
+        <h6 className="text-center">by dr.Cactus</h6>
+      </div>
+      <div className=" grid  grid-cols-3 justify-center mt-20 p-8 gap-20">
+        <div className="code-container flex-container col-span-2 ml-auto">
+          <div className="grid grid-cols-1 w-fit gap-4">
+            <p>Check your code and refactor it with AI-assistant</p>
+            <textarea
+              className="w-full h-70 border-black border-4 rounded-lg resize-none p-2"
+              name="codeReviwer"
+              id="codeReviwer"
+              placeholder="insert your code (max 500 symbols)"
+              maxLength={500}
+              onChange={controlCode}
+              value={inputValue}
+            ></textarea>
+            <button
+              type="button"
+              className="border-black border-4 rounded-lg p-2 ml-auto"
+              onClick={handleClick}
+            >
+              Send
+            </button>
+          </div>
+        </div>
+        <div className="ai-container flex flex-col gap-4">
+          <p className="text-center">Feedback of AI assistant</p>
           <textarea
-            className="w-full h-70 border-black border-4 rounded-lg"
-            name="codeReviwer"
-            id="codeReviwer"
-            placeholder="insert your code (max 500 symbols)"
-            maxLength={500}
-            onChange={controleCode}
-            value={inputValue}>
-          </textarea>
-          <button type="button" className="border-black border-4 rounded-lg p-2 ml-auto" onClick={saveCode} >Send</button>
-          <p>Check your code and refactore it with AI-assistant</p>
+            className="w-full h-70 border-black border-4 rounded-lg resize-none p-2"
+            name="aiResponse"
+            id="aiResponse"
+            placeholder="there will be feedback"
+            readOnly
+            value={ errorRequest.length !== 0 ? errorRequest : aiResponse}
+          ></textarea>
         </div>
       </div>
     </div>
   );
 };
+
+
+
